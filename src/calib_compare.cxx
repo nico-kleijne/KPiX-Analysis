1fcd5f6def7f047e659711aba61ea0e540b52264
Mengqing Wu <wumengtsing@gmail>
2018-03-01 11:53:01 +0100

    legend added for comparison code

--- src/calib_compare.cxx
+++ src/calib_compare.cxx
@@ -25,10 +25,10 @@
 #include "TKey.h"
 #include "TClass.h"
 #include "TObject.h"
+#include "TLegend.h"
 
 #include "Data.h"
 #include "DataRead.h"
-
 using namespace std;
 
 string trim(const string& str){
@@ -43,6 +43,8 @@ string trim(const string& str){
   return str.substr(first, (last - first + 1));
 }
 
+void TH1FStack(std::vector<TH1F*> th1f_arr);
+
 
 int main ( int argc, char **argv ) {
   bool printalot=true;
@@ -55,13 +57,13 @@ int main ( int argc, char **argv ) {
   string outfilename;
 
   TCanvas c1;
-  int count = 0;
-
+ 
   if (argc < 2 || argc > 3) {
     cout << "Error: wrong arguments... \n"
 	 << " [Usage]: ./calib_compare conf.txt [output.root]"<< endl;
     return 0;
   }
+
 
   if (argc == 3) outfilename = argv[2];
   else outfilename = "/scratch/data/tracker_test/analysis/comparison/calib_compare.root";
@@ -73,6 +75,8 @@ int main ( int argc, char **argv ) {
     cout<<"Unable to open the parameter files, please check: "<<argv[1]<<endl;
     exit(1);
   }
+    
+ 
   
   // check how many files to compare by counting how many lines in confFile::
   while(getline(confFile, line)) {
@@ -108,6 +112,7 @@ int main ( int argc, char **argv ) {
   std::vector<TH1F*> th1fs;
 
   // loop over all keys of 1st file as baseline:
+  int count=0;
   hfile->cd();
   while ( ( key0 = (TKey*)next0()) ) {
     th1fs.clear();
@@ -115,44 +120,66 @@ int main ( int argc, char **argv ) {
       //cout<<" key.GetName = "<< key0->GetName()<<"; title = "<<key0->GetTitle()<<endl;
       continue;
     }
-      
+
+    /*START: Legend definition*/
+    TLegend leg(0.55,0.30,0.85, 0.55);// was: 0.65,0.25,0.85,0.55 (change for tri-plot)
+    leg.SetTextSize(0.04); 
+    leg.SetTextFont(42); 
+    leg.SetTextColor(1);
+    leg.SetBorderSize(0);  //no border for legend 
+    leg.SetFillColor(0);  // white
+    leg.SetFillStyle(0); // transparent
+    /*END: Legend definition*/
+
     th1fs.push_back((TH1F*)key0->ReadObj());
     std::string title = key0->GetTitle();
     std::string name = key0->GetName();
 
-    // get all the same name plots from other files:
+    /* get all plots from other files with same object name
+     * if not exist: nullptr in the vector, keep same size as the legend vector
+     */
     for (int iobj=1; iobj<nlines; iobj++){
       //TObject* iobj_ptr = rootfiles[iobj]->Get(name.c_str());
       TH1F* iobj_ptr = (TH1F*)rootfiles[iobj]->Get(name.c_str());
-      if(iobj_ptr) th1fs.push_back(iobj_ptr);
-      else;
-    }
-
-    // plot all plots to canvas c1 and write to output file
+      th1fs.push_back(iobj_ptr);
+     }
+    if (th1fs.size()!=inlegends.size())
+      cout<<"[warn] Num of plots ("<< th1fs.size() <<") != Num of legends ("<< inlegends.size() <<")"<<endl;
+    
+    //--start - TH1F: plot all plots to canvas c1 and write to output file
     THStack *hist_comp = new THStack(title.c_str(),title.c_str());
     // Attention: below method to find xmin/max ONLY for same bin setting histograms!
     vector<int> xminbin, xmaxbin;
-    int ncolor = 0;
-    for (auto &ith1f: th1fs){
-      ith1f->SetLineWidth(2);
-      ith1f->SetLineColor(1+ncolor);
 
-      xminbin.push_back(ith1f->FindFirstBinAbove(0));
-      xmaxbin.push_back(ith1f->FindLastBinAbove(0));
+    for (unsigned int hh=0; hh<th1fs.size();hh++){
+      
+      if (!th1fs[hh]) continue; // jump over nullptr
+      
+      th1fs[hh]->SetLineWidth(2);
+      th1fs[hh]->SetLineColor(1+hh);
 
-      hist_comp->Add(ith1f);
-	
-      ncolor++;
+      leg.AddEntry(th1fs[hh], inlegends[hh].c_str(), "lp");
+      
+      xminbin.push_back(th1fs[hh]->FindFirstBinAbove(0));
+      xmaxbin.push_back(th1fs[hh]->FindLastBinAbove(0));
+
+      hist_comp->Add(th1fs[hh]);
+
     }
+    
     hist_comp->Draw("nostack");
     int xmin=*min_element(xminbin.begin(), xminbin.end()),
       xmax=*max_element(xmaxbin.begin(), xmaxbin.end());
     
     hist_comp->GetXaxis()->SetRangeUser(xmin, xmax);
+    leg.Draw("same");
     c1.Update();
     c1.Write(title.c_str());
-    	
+    //--end - TH1F: plot all plots to canvas c1 and write to output file
+    
+    count++;
   }
+  
   for (auto & fi:rootfiles ){
     fi->Close();
   }
